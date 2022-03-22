2a66510bbaed5ebb2092b35121f99d4614ff448b
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-22 23:27:33 +0100

    add two function

--- src/Controllers/routers.ts
+++ src/Controllers/routers.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from "express";
+import { signVerification } from "../Helps/validation";
 export let route = express.Router();
-route.post("/signup", (req: Request, res: Response) => {
-  let user = req.body;
-  console.log(user);
+route.post("/signup", async (req: Request, res: Response) => {
+  try {
+    let { error } = await signVerification(req.body);
+    console.log(req.body);
+    if (error) return res.status(401).send(error.details[0].message);
+
+    res.status(200).send(req.body);
+  } catch (err: any) {}
 });
--- src/Helps/validation.ts
+++ src/Helps/validation.ts
@@ -1,2 +1,19 @@
-import joi from 'joi'
+import joi from "joi";
 
+export const signVerification = async (data: object) => {
+  const schema = joi.object({
+    username: joi.string().alphanum().min(3).max(20),
+    email: joi.string().email(),
+    password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+    birthday: joi.date().greater("1-1-1970"),
+  });
+  return schema.validate(data);
+};
+
+export const loginVerification = async (data: object) => {
+  const schema = joi.object({
+    email: joi.string().email(),
+    password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  });
+  return schema.validate(data);
+};
