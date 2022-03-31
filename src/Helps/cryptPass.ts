bc12da61d8fa56297d255baebf48771d143b18f5
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-03-31 22:28:52 +0100

    ...

--- src/Helps/cryptPass.ts
+++ src/Helps/cryptPass.ts
@@ -1,7 +1,18 @@
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 export async function hashing_password(password: string) {
   let salt: any = await bcrypt.genSalt(10);
   let hash = bcrypt.hash(password, salt);
   return hash;
+}
+
+export function compare(origin_password: string, input_password: string) {
+  const cmp = bcrypt.compare(
+    input_password,
+    origin_password,
+    (err: any, res: any) => {
+      if (!res) return "invalid password";
+    }
+  );
+  return cmp;
 }
