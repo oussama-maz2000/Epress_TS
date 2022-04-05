c384e446831093e601dbf46e339202fcc152dd27
Mazeghrane Oussama <loulououssama2015@gmail.com>
2022-04-05 20:18:14 +0100

    ...

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,22 @@ import * as bodyParser from "body-parser";
 import { connect } from "./Model/connection";
 import { route } from "./Controllers/routersUser";
 import { route_store } from "./Controllers/routersStore";
+import globaleErr from "./Errors/Err_Middlware";
 const app = express();
 app.use(express.json());
 dotenv.config();
 
 connect();
-app.set("view engine", "hbs");
+//app.set("view engine", "hbs");
 app.use(route);
 app.use("/store", route_store);
+app.use(globaleErr);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send("welcome ");
 });
+
 app.listen(3001, () => {
   console.log("server started on port 3001");
 });
+
